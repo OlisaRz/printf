afac4e7379d9493296ec717e0a4b8f62c3750e26
Rosemary Okonda <olisaokonda@gmail.com>
2023-07-25 22:26:24 +0000

    updated main.h file

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.hi>
+#include <stdarg.h>
 
+/*prototype for _printf file*/
 int _printf(const char *format, ...);
+
 int_print_binary(unsigned int num);
 
 #endif /*end of MAIN_H*/
