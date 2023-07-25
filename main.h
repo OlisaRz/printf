277b9bc4fe6a378a56f9ac6977e9c7c428718e93
Rosemary Okonda <olisaokonda@gmail.com>
2023-07-25 22:28:05 +0000

    updated main.h file

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 /*prototype for _printf file*/
 int _printf(const char *format, ...);
 
+/*our written prototype for task on binary*/
 int_print_binary(unsigned int num);
 
 #endif /*end of MAIN_H*/
