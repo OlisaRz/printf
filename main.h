139d52d518565cc88580316d75e045d771a3fbf3
Rosemary Okonda <olisaokonda@gmail.com>
2023-07-25 22:44:42 +0000

    updated main.h file

--- main.h
+++ main.h
@@ -2,10 +2,7 @@
 #define MAIN_H
 #include <stdarg.h>
 
-/*prototype for _printf file*/
 int _printf(const char *format, ...);
-
-/*our written prototype for task on binary*/
 int_print_binary(unsigned int num);
 
-#endif /*end of MAIN_H*/
+#endif
