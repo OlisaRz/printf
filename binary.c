a65f19d789e18c593fb12f374d44f2bdd459e48b
Rosemary Olisa <103603536+OlisaRz@users.noreply.github.com>
2023-07-27 19:02:57 +0300

    Delete binary.c

--- binary.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-/**
- * _print_binary - Prints an unsigned integer in binary
- * @num: The number to print
- * Return: The numberof characters printed
- */
-int _print_binary(unsigned int num)
